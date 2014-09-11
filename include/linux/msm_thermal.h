24485771b8d8d4c4c2c5434d9d816dc96b6c7f7f
srsdanitest <srsdanitest@gmail.com>
2014-09-12 01:22:20 +0300

    intelli/msm_thermal: initial coding for Qualcomm S4 Pro devices an intelligent MSM thermal throttling kernel driver designed to replace the closed sourced thermald(aemon) from Qualcomm.
    The current driver can throttle the SOC via core frequency scaling or via
    core hotplug with user configurable sysfs entries to adjust the throttling
    temperature thresholds for each.
    
    Derived from Qualcomm's Open source repo.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -18,11 +18,14 @@ struct msm_thermal_data {
 	uint32_t sensor_id;
 	uint32_t poll_ms;
 	uint32_t limit_temp;
+#ifdef CONFIG_INTELLI_THERMAL
+	uint32_t freq_control_mask;
+#endif
 	uint32_t temp_hysteresis;
 	uint32_t limit_freq;
 };
 
-#ifdef CONFIG_THERMAL_MONITOR
+#if defined(CONFIG_THERMAL_MONITOR) || defined(CONFIG_INTELLI_THERMAL)
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 #else
 static inline int msm_thermal_init(struct msm_thermal_data *pdata)
